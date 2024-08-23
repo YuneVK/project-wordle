b087f2b719435823059dad2572e588de927639f7
Sonia Ruiz <yune.vk@gmail.com>
2024-08-23 12:07:44 +0200

    ✨ Exercise 1: GuessInput

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+
+function GuessInput({ onGuessChange }) {
+  const [guess, setGuess] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    setGuess("")
+
+    console.log({ guess })
+  }
+
+  const handleGuessChange = (event) => {
+    setGuess(event.target.value.toUpperCase())
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleGuessChange}
+        pattern="^[A-Za-z]{5}$"
+        title="Must be exactly 5 alphanumeric characters"
+        required
+      />
+    </form>
+  )
+}
+
+export default GuessInput
